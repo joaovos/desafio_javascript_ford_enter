52e1e438d92c347ef37178ee04cad1d741a0df50
joaovos <joao.jvos22@gmail.com>
2026-07-29 23:53:39 -0300

    ajustes iniciais no carousel

--- js/carousel.js
+++ js/carousel.js
@@ -9,6 +9,11 @@ let carouselArr = [];
 //class Carousel
 class Carousel {
 
+    constructor(image, title, url) {
+        this.image = image;
+        this.title = title;
+        this.url = url;
+    }
     
       
     static Start(arr){
@@ -17,8 +22,9 @@ class Carousel {
             if(arr.length > 0){
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
+                Carousel._arr = arr;
                 Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
+                Carousel._interval = setInterval(function(){ Carousel.Next(); },2000);
             }
             
         } else {
@@ -26,7 +32,41 @@ class Carousel {
         }
     }
 
-    static Next(){
-        
+   static Next() {
+
+
+
+let carouselDiv = document.getElementById("carousel");
+let titleDiv = document.getElementById("carousel-title");
+
+
+
+
+
+
+
+let currentItem = Carousel._arr[Carousel._sequence];
+
+
+
+
+if (currentItem) {
+    let img = carouselDiv.querySelector("img");
+
+    if (!img) {
+        img = document.createElement("img");
+        carouselDiv.appendChild(img);
     }
+
+    img.src = currentItem.image;
+    img.alt = currentItem.title;
+    titleDiv.innerHTML = `<a href="${currentItem.url}">${currentItem.title}</a>`;
+}
+
+
+
+
+
+Carousel._sequence = (Carousel._sequence + 1) % Carousel._size; 
 };
+}
